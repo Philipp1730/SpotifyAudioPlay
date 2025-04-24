3be0fb5e7bfbb7d7c30917f3caa6b5a655c5a716
Philipp Müller <82723256+Philipp1730@users.noreply.github.com>
2025-04-24 16:59:08 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,17 +2,19 @@
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('my-cache').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/styles.css',
-        '/script.js',
-        '/images/icon-192x192.png',
-        '/images/icon-512x512.png'
-      ]);
-    })
-  );
-});
+  return cache.addAll([
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/script.js',
+    '/images/icon-192x192.png',
+    '/images/icon-512x512.png'
+  ]).catch((error) => {
+    console.error('Failed to cache resources:', error);
+  });
+ });
+})
+
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
